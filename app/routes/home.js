1dca84ea9ec0cefb92932096ca6a13c0bcaa8e71
vhlaynez <35643243+vhlaynez@users.noreply.github.com>
2018-01-21 17:39:04 -0200

    Update home.js
    tirado var controller

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,5 @@
 module.exports = function(application){
-	var controller = require('../controller/home');
-	
+		
 	application.get('/', function(req, res){
 		console.log('estou na home route');
 		application.app.controllers.home.index(application, req, res);
