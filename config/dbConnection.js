755d5598ee2fbeb0d3dc44ff9ebf430b94459754
Victor <vhlaynez@hotmail.com>
2018-01-23 13:48:18 -0200

    removido configurações do banco de dados

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,13 +2,12 @@ var mysql = require('mysql');
 	
 var connMySQL = function(){
 	console.log('conexão com o bd foi realizada');
-	return mysql.createConnection(JAWSDB_URL);
-	/*return mysql.createConnection({
-		host:'mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-		user:'uhyogic5144ah01v',
-		password:'znk732ge6m11vy61',
-		database:'jjs0ervflrpdv07n'
-	});*/
+	return mysql.createConnection({
+		host:'',
+		user:'',
+		password:'',
+		database:''
+	});
 }
 
 module.exports = function(){
